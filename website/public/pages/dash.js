67048767b89c1d5022f667778e913b0dab17de47
David Middlecamp <david@spark.io>
2015-09-23 12:52:44 -0500

    basic dygraph styling

--- website/public/pages/dash.js
+++ website/public/pages/dash.js
@@ -17,179 +17,121 @@
 	var Page = function() {};
 	Page.prototype = {
 		init: function() {
-			this.makeTubChart3();
-			//this.makeTubChart2();
-
-			//this.makeWeatherChart();
-			this.makeWeatherChart2();
-
-			//this.makeCoopChart();
-			this.makeCoopChart2();
+			this.makeTubChart();
+			this.makeWeatherChart();
+			this.makeCoopChart();
 
 			console.log("loaded!");
 		},
 
 		/**
-		 * using c3.js
-		 */
-		makeTubChart: function() {
-			this.tubChart = c3.generate({
-				bindto: "#tubChart",
-				data: {
-					mimeType: 'json',
-					url: '/data/tub.json',
-					keys: {
-						x: 'published_at',
-						value: ['temp']
-					}
-				},
-				axis: {
-					x: {
-						type: 'timeseries',
-						tick: {
-							format: '%a, %m-%d %H:%M'
-						}
-					}
-				}
-			});
-		},
-
-		/**
-		 * using c3.js
+		 * using dygraphs
 		 */
 		makeCoopChart: function() {
-			this.coopChart = c3.generate({
-				bindto: "#coopChart",
-				data: {
-					mimeType: 'json',
-					url: '/data/coop.json',
-					keys: {
-						x: 'published_at',
-						value: ['temp']
-					}
-				},
-				axis: {
-					x: {
-						type: 'timeseries',
-						tick: {
-							format: '%a, %m-%d %H:%M'
-						}
-					}
-				}
-			});
-		},
-
-		/**
-		 * using dygraphs
-		 */
-		makeCoopChart2: function() {
 			this.coopChart = new Dygraph(
-				// containing div
 				document.getElementById("coopChart"),
-				"/data/coop.csv"
+				"/data/coop.csv",
+				{
+					//errorBars: true
+				}
 			);
 		},
 
 		/**
 		 * using dygraphs
 		 */
-		makeTubChart3: function() {
+		makeTubChart: function() {
 			this.tubChart = new Dygraph(
-				// containing div
 				document.getElementById("tubChart"),
-				"/data/tub.csv"
+				"/data/tub.csv",
+				{
+					//errorBars: true
+				}
 			);
 		},
 
 		/**
 		 * using dygraphs
 		 */
-		makeWeatherChart2: function() {
+		makeWeatherChart: function() {
 			this.weatherChart = new Dygraph(
-				// containing div
 				document.getElementById("weatherChart"),
 				"/data/weather.csv"
 			);
 		},
 
-		/**
-		 * using c3.js
-		 */
-		makeWeatherChart: function() {
-			this.weatherChart = c3.generate({
-				bindto: "#weatherChart",
-				data: {
-					mimeType: 'json',
-					url: '/data/weather.json',
-					keys: {
-						x: 'published_at',
-						value: [
-							'temp1',
-							'temp2',
-							'humidity',
-							'pressure',
-							'altitude',
-							'wind_mph',
-							'soilTemp'
-						]
-					}
-				},
-				axis: {
-					x: {
-						type: 'timeseries',
-						tick: {
-							format: '%a, %m-%d %H:%M'
-						}
-					}
-				}
-			});
-		},
-
-		/**
-		 * using chart.js
-		 */
-		makeTubChart2: function() {
-			var makeChart = function(labels, series) {
-				var data = {
-					labels: labels,
-					datasets: [
-						{
-							label: "My First dataset",
-							fillColor: "rgba(220,220,220,0.2)",
-							strokeColor: "rgba(220,220,220,1)",
-							pointColor: "rgba(220,220,220,1)",
-							pointStrokeColor: "#fff",
-							pointHighlightFill: "#fff",
-							pointHighlightStroke: "rgba(220,220,220,1)",
-							data: series
-						}
-					]
-				};
-
-				var ctx = $("#tubChart2").get(0).getContext("2d");
-				var tubChart2 = new Chart(ctx).Line(data);
-			};
-
-			$.ajax({
-				url: "/data/tub.json",
-				success: function(data) {
-					var labels = [];
-					var series = [];
-
-					for(var i = 0; i < data.length; i++) {
-						var d = data[i];
-
-						var timeLabel = moment(d.published_at).format("ddd, h:mm A");
-
-						labels.push(timeLabel);
-						series.push(d.temp);
-					}
-					makeChart(labels, series);
-				}
-			});
 
 
-		},
+		//saved as a reference
+
+//		/**
+//		 * using c3.js
+//		 */
+//		makeTubChart: function() {
+//			this.tubChart = c3.generate({
+//				bindto: "#tubChart",
+//				data: {
+//					mimeType: 'json',
+//					url: '/data/tub.json',
+//					keys: {
+//						x: 'published_at',
+//						value: ['temp']
+//					}
+//				},
+//				axis: {
+//					x: {
+//						type: 'timeseries',
+//						tick: {
+//							format: '%a, %m-%d %H:%M'
+//						}
+//					}
+//				}
+//			});
+//		},
+//
+//		/**
+//		 * using chart.js
+//		 */
+//		makeTubChart2: function() {
+//			var makeChart = function(labels, series) {
+//				var data = {
+//					labels: labels,
+//					datasets: [
+//						{
+//							label: "My First dataset",
+//							fillColor: "rgba(220,220,220,0.2)",
+//							strokeColor: "rgba(220,220,220,1)",
+//							pointColor: "rgba(220,220,220,1)",
+//							pointStrokeColor: "#fff",
+//							pointHighlightFill: "#fff",
+//							pointHighlightStroke: "rgba(220,220,220,1)",
+//							data: series
+//						}
+//					]
+//				};
+//
+//				var ctx = $("#tubChart2").get(0).getContext("2d");
+//				var tubChart2 = new Chart(ctx).Line(data);
+//			};
+//
+//			$.ajax({
+//				url: "/data/tub.json",
+//				success: function(data) {
+//					var labels = [];
+//					var series = [];
+//
+//					for(var i = 0; i < data.length; i++) {
+//						var d = data[i];
+//
+//						var timeLabel = moment(d.published_at).format("ddd, h:mm A");
+//
+//						labels.push(timeLabel);
+//						series.push(d.temp);
+//					}
+//					makeChart(labels, series);
+//				}
+//			});
+//		},
 
 
 		_: null
