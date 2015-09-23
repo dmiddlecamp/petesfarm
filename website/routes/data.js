9a78be6cfa45015b259e492c97afb085165b11a6
David Middlecamp <david@spark.io>
2015-09-23 15:11:58 -0500

    standardize query interface

--- website/routes/data.js
+++ website/routes/data.js
@@ -17,43 +17,65 @@ router.get('/', function(req, res, next) {
 });
 
 var DataLogic = {
-	coopQuery: function(req) {
-		var query = "select temp, published_at from coop order by published_at";
-		return Database.query(query).then(function(records) {
-			records = Formatters.bruteForceFilter(records, "temp", -100, 200);
-			return when.resolve(records);
-		});
-	},
-	tubQuery: function(req) {
-		var query = "select temp, published_at from tub order by published_at";
-		return Database.query(query).then(function(records) {
-			return when.resolve(records);
-		});
-	},
-	weatherQuery: function(req) {
-		var numRows = 4320;
+	/**
+	 * encapsulates common API query params
+	 *
+	 * count, sort, columns, and more!
+	 *
+	 * @param req
+	 * @param tableName
+	 * @param orderColumn
+	 * @param allowedColumns
+	 * @returns {*}
+	 */
+	dbQuery: function(req, tableName, orderColumn, allowedColumns) {
+		var numRows = parseInt(req.param("count", 4320));
 
+		var ascDesc = req.param("sort", "ASC").substring(0, 4);
 
-		var allColumns = "temp1,temp2,humidity,pressure,altitude,wind_mph,soilTemp,rain,published_at".split(",");
-		var userColumns = allColumns;
-
-
+		var userColumns = allowedColumns;
 		var columnsParam = req.param("columns");
 		if (columnsParam) {
 			userColumns = columnsParam.split(",");
 
 			//sanitize your inputs kids!
-			userColumns = utilities.filterListInList(allColumns, userColumns);
+			userColumns = utilities.filterListInList(allowedColumns, userColumns);
 		}
 
-		var query = "select top 4320  " + userColumns.join(", ") +
-			" from weather order by published_at";
 
-		return Database.query(query).then(function(records) {
-			records = Formatters.bruteForceSmooth(records, "soilTemp", -150, 150);
 
-			return when.resolve(records);
-		});
+		var query = "select "
+			+ "top " + numRows + " "
+			+ userColumns.join(", ") +
+			" from " + tableName + " order by " + orderColumn + " " + ascDesc;
+
+		return Database.query(query);
+	},
+
+	coopQuery: function(req) {
+		var allowedColumns = "temp,published_at".split(",");
+		return DataLogic.dbQuery(req, "coop", "published_at", allowedColumns)
+			.then(function(records) {
+				records = Formatters.bruteForceFilter(records, "temp", -100, 200);
+				return when.resolve(records);
+			});
+
+	},
+	tubQuery: function(req) {
+		var allowedColumns = "temp,published_at".split(",");
+		return DataLogic.dbQuery(req, "tub", "published_at", allowedColumns)
+			.then(function(records) {
+				return when.resolve(records);
+			});
+	},
+	weatherQuery: function(req) {
+		var allowedColumns = "temp1,temp2,humidity,pressure,altitude,wind_mph,soilTemp,rain,published_at".split(",");
+		return DataLogic.dbQuery(req, "weather", "published_at", allowedColumns)
+			.then(function(records) {
+				records = Formatters.bruteForceSmooth(records, "soilTemp", -150, 150);
+
+				return when.resolve(records);
+			});
 	}
 };
 
@@ -104,8 +126,6 @@ router.get('/tub.csv', function(req, res, next) {
 			res.send({ message: "something went wrong!", error: err });
 		});
 });
-
-
 
 
 router.get('/weather.json', function(req, res, next) {
