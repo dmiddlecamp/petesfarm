b42e4564abf3df0cb1b33437a289ecba10755ab6
David Middlecamp <david@spark.io>
2015-09-22 23:58:57 -0500

    stubbing out test graphs in c3 / chart.js, testing speed.

--- website/public/pages/dash.js
+++ website/public/pages/dash.js
@@ -1,6 +1,15 @@
 /**
  * Created by middleca on 9/22/15.
+ *
+ * Notes:
+ *
+ * * C3 is slow, and a little picky.
+ * * chart.js is slow, and doesn't scale the x axis properly, doesn't understand the time, just thinks they're labels
+ * * maybe chart.js / c3 are better fits for very small summary datasets?
+ * [ ]  should try http://dygraphs.com/ next.
  */
+
+
 
 (function() {
 	/* source for the dash page */
@@ -9,10 +18,17 @@
 	Page.prototype = {
 		init: function() {
 			this.makeTubChart();
+			this.makeTubChart2();
+
+			this.makeWeatherChart();
+			this.makeCoopChart();
 
 			console.log("loaded!");
 		},
 
+		/**
+		 * using c3.js
+		 */
 		makeTubChart: function() {
 			this.tubChart = c3.generate({
 				bindto: "#tubChart",
@@ -33,8 +49,119 @@
 					}
 				}
 			});
+		},
+
+		/**
+		 * using c3.js
+		 */
+		makeCoopChart: function() {
+			this.coopChart = c3.generate({
+				bindto: "#coopChart",
+				data: {
+					mimeType: 'json',
+					url: '/data/coop.json',
+					keys: {
+						x: 'published_at',
+						value: ['temp']
+					}
+				},
+				axis: {
+					x: {
+						type: 'timeseries',
+						tick: {
+							format: '%a, %m-%d %H:%M'
+						}
+					}
+				}
+			});
+		},
+
+			/**
+		 * using c3.js
+		 */
+		makeWeatherChart: function() {
+			this.weatherChart = c3.generate({
+				bindto: "#weatherChart",
+				data: {
+					mimeType: 'json',
+					url: '/data/weather.json',
+					keys: {
+						x: 'published_at',
+						value: [
+							'temp1',
+							'temp2',
+							'humidity',
+							'pressure',
+							'altitude',
+							'wind_mph',
+							'soilTemp'
+						]
+
+
+					}
+				},
+				axis: {
+					x: {
+						type: 'timeseries',
+						tick: {
+							format: '%a, %m-%d %H:%M'
+						}
+					}
+				}
+			});
+		},
+
+		/**
+		 * using chart.js
+		 */
+		makeTubChart2: function() {
+			var makeChart = function(labels, series) {
+				var data = {
+					labels: labels,
+					datasets: [
+						{
+							label: "My First dataset",
+							fillColor: "rgba(220,220,220,0.2)",
+							strokeColor: "rgba(220,220,220,1)",
+							pointColor: "rgba(220,220,220,1)",
+							pointStrokeColor: "#fff",
+							pointHighlightFill: "#fff",
+							pointHighlightStroke: "rgba(220,220,220,1)",
+							data:  series
+						}
+					]
+				};
+
+				var ctx = $("#tubChart2").get(0).getContext("2d");
+				var tubChart2 = new Chart(ctx).Line(data);
+			};
+
+			$.ajax({
+				url: "/data/tub.json",
+				success: function(data) {
+					var labels = [];
+					var series = [];
+
+					for(var i=0;i<data.length;i++) {
+						var d = data[i];
+
+						var timeLabel = moment(d.published_at).format("ddd, h:mm A");
+
+						labels.push(timeLabel);
+						series.push(d.temp);
+					}
+					makeChart(labels,series);
+				}
+			});
+
+
+
+
+
 
 		},
+
+
 		_: null
 	};
 
--- website/routes/data.js
+++ website/routes/data.js
@@ -7,41 +7,44 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	res.send({
 		routes: [
-			{ url: "coop", desc: "chicken coop temperature / sound data" },
-			{ url: "tub", desc: "hot tub temperature data" },
-			{ url: "weather", desc: "weather station data" }
+			{ url: "coop.json", desc: "chicken coop temperature / sound data" },
+			{ url: "tub.json", desc: "hot tub temperature data" },
+			{ url: "weather.json", desc: "weather station data" }
 		]
 	});
 });
 
-router.get('/coop', function(req, res, next) {
-
-	Database.query("select * from coop").then(function(records) {
-		res.send("got some records! ");
+router.get('/coop.json', function(req, res, next) {
+	var query = "select temp, published_at from coop order by published_at";
+	Database.query(query).then(function(records) {
+		records = Formatters.timeCop(records, "published_at");
+		res.json(records);
 	}, function(err) {
 		res.send("something went wrong!");
 	});
-
-	//res.send('respond with a resource coop');
 });
 
 router.get('/tub.json', function(req, res, next) {
 	var query = "select temp, published_at from tub order by published_at";
-		Database.query(query).then(function(records) {
-			records = Formatters.timeCop(records, "published_at");
-			res.json(records);
-		}, function(err) {
-			res.send("something went wrong!");
-		});
-
+	Database.query(query).then(function(records) {
+		records = Formatters.timeCop(records, "published_at");
+		res.json(records);
+	}, function(err) {
+		res.send("something went wrong!");
+	});
 });
 
-router.get('/tub', function(req, res, next) {
-	res.send('respond with a resource tub');
-});
+router.get('/weather.json', function(req, res, next) {
+		var query = "select top 4320 temp1, temp2, humidity, pressure, altitude, wind_mph, soilTemp, published_at " +
+			" from weather order by published_at";
+	Database.query(query).then(function(records) {
+		records = Formatters.timeCop(records, "published_at");
+		res.json(records);
+	}, function(err) {
+		res.send("something went wrong!");
+	});
 
-router.get('/weather', function(req, res, next) {
-	res.send('respond with a resource weather');
+
 });
 
 module.exports = router;
