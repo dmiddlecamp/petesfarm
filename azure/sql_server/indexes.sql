509378b6cd50ac667767637da1c001f2d83b281f
David Middlecamp <david@spark.io>
2015-10-11 21:58:02 -0500

    index notes

---
+++ azure/sql_server/indexes.sql
@@ -0,0 +1,5 @@
+CREATE NONCLUSTERED INDEX idx_coop_published_at ON coop (published_at);
+
+CREATE NONCLUSTERED INDEX idx_tub_published_at ON tub (published_at);
+
+CREATE NONCLUSTERED INDEX idx_weather_published_at ON weather (published_at);
